128cdb1a8ff0a13ba4f27d37be98c9406c5dd466
Gaetano Re <gaetanore@live.com>
2022-01-12 13:14:01 -0500

    Everything from Tutorial 3 has been added. For some people, the FPS may say Infinity. I have this problem too. I looked in the comments and he says he switches what he uses for time later in the series since nanoTime() doesn't work right.

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -3,6 +3,7 @@ package jade;
 import org.lwjgl.*;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
+import util.*;
 
 import static java.sql.Types.NULL;
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
@@ -12,10 +13,13 @@ import static org.lwjgl.opengl.GL11.*;
 public class Window {
     private int width, height;
     private String title;
-    private static Window window;
+    private static Window window = null;
     private long glfwWindow;
-    private float r, g, b, a;
+    public float r, g, b, a;
     private boolean fadeToBlack = false;
+
+    private static Scene currentScene = null;
+
 
     private Window() {
         this.width = 1920;
@@ -25,6 +29,23 @@ public class Window {
         g = 1;
         b = 1;
         a = 1;
+    }
+
+    public static void changeScene(int newScene){
+        switch(newScene){
+            case 0:
+                currentScene = new LevelEditorScene();
+                //currentScene.init();
+                break;
+
+            case 1:
+                currentScene = new LevelScene();
+                break;
+
+            default:
+                assert false : "Unknown scene " + newScene + "'";
+                break;
+        }
     }
 
     public static Window get() {
@@ -88,25 +109,29 @@ public class Window {
 		bindings available for use.
 		 */
         GL.createCapabilities();
+
+        Window.changeScene(0);
     }
 
     public void loop() {
+
+        float beginTime = Time.getTime();
+        float endTime;
+        float dt = -1.0f;
+
         while (!glfwWindowShouldClose(glfwWindow)){
             glfwPollEvents();
 
             glClearColor(r, g, b, a);
             glClear(GL_COLOR_BUFFER_BIT);
+            if(dt >= 0)
+                currentScene.update(dt);
 
-            if(fadeToBlack){
-                r = Math.max(r - 0.01f, 0);
-                g = Math.max(g - 0.01f, 0);
-                b = Math.max(b - 0.01f, 0);
-            }
-            if(KeyListener.isKeyPressed(GLFW_KEY_SPACE)){
-                fadeToBlack = true;
-            }
 
             glfwSwapBuffers(glfwWindow);
+            endTime = Time.getTime();
+            dt = endTime - beginTime;
+            beginTime = endTime;
         }
     }
 
